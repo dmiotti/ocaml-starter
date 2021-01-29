d16945a1427302a3086146d7539d250794c13d60
David Miotti <david@station-coding.com>
2021-01-30 00:19:08 +0100

    app.ml: returns 422 when query paramters are wrong

--- web/app.ml
+++ web/app.ml
@@ -1,18 +1,21 @@
 open Opium
 open Lib
 
-let add_handler req =
-  let a = (int_of_string (Request.query_exn "a" req)) in
-  let b = (int_of_string (Request.query_exn "b" req)) in
-  let add = Math.add a b in
-  Lwt.return (Response.of_plain_text (string_of_int add))
+let bad_request_response (reason : string) =
+  Response.of_plain_text reason |> Response.set_status `Bad_request
 
-let health_check_handler _ =
-  Lwt.return (Response.of_plain_text "I'm healthy.")
+let add_handler req =
+    try
+        let a = Request.query_exn "a" req in
+        let b = Request.query_exn "b" req in
+        let result = Math.add (int_of_string a) (int_of_string b) in
+        Lwt.return (Response.of_plain_text (string_of_int result))
+    with Invalid_argument(error) -> Lwt.return (bad_request_response error)
+
+let health_check_handler _ = Lwt.return (Response.of_plain_text "I'm healthy.")
 
 let run () =
   print_endline "Running server at localhost:3000";
   App.empty
   |> App.get "/" health_check_handler
-  |> App.get "/add" add_handler
-  |> App.run_command
+  |> App.get "/add" add_handler |> App.run_command
